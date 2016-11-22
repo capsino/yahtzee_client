962af1dc2c6e95a7b2d2131fc0e724b1a8d40c1f
John <john.eccleston@capgemini.com>
2016-11-22 15:46:37 +0000

    reorganise structure

--- docker-toolbox-run.sh
+++ docker-toolbox-run.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-client_ip=$(docker-machine ip default)
-server_ip=$(docker-machine ip default)
-
-curl --include --header "Accept: application/json" http://${client_ip}:80
-echo
-curl --include --header "Accept: application/json" http://${server_ip}:4567
+ip=$(docker-machine ip default)
+curl --include --header "Accept: application/json" http://${ip}:80
 echo
