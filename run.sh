6f759b9be6c9b05c56f2bed8565105b750dd07ab
John <john.eccleston@capgemini.com>
2016-11-23 10:40:17 +0000

    correct port to 80 in run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 ip=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' yahtzee_client)
-curl --include --header "Accept: application/json" http://${ip}:4567
+curl --include --header "Accept: application/json" http://${ip}:80
 echo
