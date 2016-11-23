ad06e46decd80ae85cb31e09e26b6d3ea5ba4b7e
John <john.eccleston@capgemini.com>
2016-11-23 10:34:05 +0000

    change service name in compose file and container name in run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-ip=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' spark_client)
+ip=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' yahtzee_client)
 curl --include --header "Accept: application/json" http://${ip}:4567
 echo
