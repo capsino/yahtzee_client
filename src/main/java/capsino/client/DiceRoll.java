32f2e72d1352d0361a802c2d4ee6ed5193079236
James Relph <james.relph@capgemini.com>
2016-11-24 15:38:20 +0000

    Test getting array

--- src/main/java/capsino/client/DiceRoll.java
+++ src/main/java/capsino/client/DiceRoll.java
@@ -2,13 +2,13 @@ package capsino.client;
 
 public class DiceRoll {
 
-	private int roll;
+	private int[] roll = new int[5];
 
 	public int getRoll() {
 		return roll;
 	}
 
-	public void setRoll(int roll) {
+	public void setRoll(int[] roll) {
 		this.roll = roll;
 	}
 
